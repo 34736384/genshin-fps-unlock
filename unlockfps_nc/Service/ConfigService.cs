3a935a8e402794e3a76005ea46ab11182d7bdff0
34736384 <34736384@users.noreply.github.com>
2025-09-27 22:58:59 -0400

    fix #547

--- unlockfps_nc/Service/ConfigService.cs
+++ unlockfps_nc/Service/ConfigService.cs
@@ -57,9 +57,26 @@ namespace unlockfps_nc.Service
                 var configPath = GetFullPath();
                 var json = JsonSerializer.Serialize(Config, new JsonSerializerOptions { WriteIndented = true });
 
-                using var fs = new FileStream(configPath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, FileOptions.WriteThrough);
-                using var sw = new StreamWriter(fs, Encoding.UTF8);
-                sw.Write(json);
+                bool wasHidden = false;
+                if (File.Exists(configPath)) {
+                    var attributes = File.GetAttributes(configPath);
+                    if ((attributes & FileAttributes.Hidden) != 0) {
+                        wasHidden = true;
+                        File.SetAttributes(configPath, attributes & ~FileAttributes.Hidden);
+                    }
+                }
+
+                try {
+                    using var fs = new FileStream(configPath, FileMode.Create, FileAccess.Write, FileShare.None, 4096, FileOptions.WriteThrough);
+                    using var sw = new StreamWriter(fs, Encoding.UTF8);
+                    sw.Write(json);
+                }
+                finally {
+                    if (wasHidden && File.Exists(configPath)) {
+                        var attributes = File.GetAttributes(configPath);
+                        File.SetAttributes(configPath, attributes | FileAttributes.Hidden);
+                    }
+                }
             }
 
         }
